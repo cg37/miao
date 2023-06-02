8bcc2771f15fb0f56ca9996095e6d5466878bf86
cg37 <cg37@outlook.com>
2023-06-02 20:44:55 +0800

    two

--- lodash/cg37-lodash.js
+++ lodash/cg37-lodash.js
@@ -1,11 +1,19 @@
 var cg37 = {
-  compact:function (array, size) {
+  compact:function (array) {
     for (let i = 0; i < array.length; i++) {
       if(!Boolean(array[i])) {
         array.splice(i,1)
       }
     }
     return array
+  },
+  chunk:function(array, size) {
+    let res = []
+    for (let i = 0; i < array.length + size; i = i + size) {
+      res.push(array.slice(0, size))
+      array.splice(i, size)
+    }
+    return res
   }
-
 }
+console.log(cg37.chunk(['a', 'b', 'c', 'd'], 3))
