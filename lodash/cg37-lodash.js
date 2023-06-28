cf9a5a99d73e6620f7894c603644ea1a3c4b9d27
cg37 <cg37@outlook.com>
2023-06-28 21:03:11 +0800

    lodash

--- lodash/cg37-lodash.js
+++ lodash/cg37-lodash.js
@@ -1,14 +1,4 @@
 var cg37 = {
-  compact:function (array) {
-    let res = []
-    for (let i of array) {
-      if (i) {
-        res.push(i)
-      }
-    }
-    return res
-  },
-
   chunk:function(array, size) {
     let res = []
     let len = array.length
@@ -19,6 +9,29 @@ var cg37 = {
     }
     return res
   },
+
+  compact:function (array) {
+    let res = []
+    for (let i of array) {
+      if (i) {
+        res.push(i)
+      }
+    }
+    return res
+  },
+
+  /**
+   *
+   * inspectArray, 要检查的数组
+   * excludeArray, 要排除的数组
+   *
+   *
+   *
+  */
+  difference: function(inspectArray, excludeArray) {
+
+  },
+
 
   fill:function(array, val, start = 0, end = array.length) {
     for (let i = start; i < end; i++) {
@@ -31,38 +44,29 @@ var cg37 = {
     return array.slice(n)
   },
 
+
   findIndex:function(array, predicate, fromIndex = 0) {
+    let func = iteratee(predicate)
+
     let length = array == null ? 0 : array.length
-    if (!length) {
-      return -1
-    }
-
-    let index = fromIndex == null ? 0 : parseInt(fromIndex);
-    if (index < 0) {
-      index = Math.max(length + index, 0)
-    }
-    return baseFindIndex(array, getIteratee(predicate, 3), index)
-  },
-
-  baseFindIndex: function(array, predicate, fromIndex, fromRight) {
-    const{length } = array
-    let index =fromIndex + (fromIndex ? 1 : -1)
-
-    while((fromRight ? index-- : ++index < length)) {
-      if (predicate(array[index], index, array)) {
-        return index
+    for (let i = fromIndex; i < length; i++) {
+      if(func(array[i])) {
+        return i
       }
     }
     return -1
   },
 
+  findLastIndex:function(array, predicate, startLastIndex = array.length == null ? 0 : array.length) {
+    let func = iteratee(predicate)
 
-
-  toInteger: function(value) {
-
+    let length = array == null ? 0 : array.length
+    for(let i = startLastIndex - 1; i >= 0; i--){
+      if(func(array[i])) {
+        return i
+      }
+    }
   },
-
-  findLastIndex:function(array) {},
 
   flatten:function(array) {
     let flatten = []
@@ -80,7 +84,7 @@ var cg37 = {
     let res = []
     for (let i of array) {
       if (Array.isArray(i)) {
-        res = res.concat(flattenDeep(i))
+        res = res.concat(this.flattenDeep(i))
       } else {
         res.push(i)
       }
@@ -227,6 +231,11 @@ var cg37 = {
 
   },
 
+
+  proerty:function (path) {
+
+  }
+
         // isUndefined,isNull,isNil,max,min,maxBy,minBy,round,sumBy
         // flagMap,flatMapDepth,get,has,mapKeys,mapValues
         // range,stringifyJSON,concat,isEqual,repeat,padStart,padEnd,pad,keys,values,random,
@@ -237,4 +246,219 @@ var cg37 = {
 
 
 
+
+  function sum(arr) {
+    return array.reduce((a,b) => a + b)
+  }
+
+  function sumBy(array, iterator) {
+    let res = 0
+    for (let i = 0; i < array.length; i++) {
+      res = res + iterator(array[i])
+    }
+    return res
+  }
+
+  // var users = [
+  //   { 'user': 'barney', 'age': 36, 'active': true },
+  //   { 'user': 'fred',   'age': 40, 'active': false }
+  // ];
+
+  // _.filter(users, function(o) { return !o.active; });
+  // // => objects for ['fred']
+
+  // // The `_.matches` iteratee shorthand.
+  // _.filter(users, { 'age': 36, 'active': true });
+  // // => objects for ['barney']
+
+  // // The `_.matchesProperty` iteratee shorthand.
+  // _.filter(users, ['active', false]);
+  // // => objects for ['fred']
+
+  // _.filter(users, 'active');
+
+  function filter(array, predicate) {
+    let res = []
+    if (typeof predicate === 'string') { //_.filter(users, 'active');
+      for (let item of array) {
+        if (item[predicate]) {
+          res.push(item)
+        }
+      }
+    } else if (Array.isArray(predicate)) { //_.filter(users, ['active', false]);
+      for (let item of array) {
+        if (item[predicate[0]] === predicate[1]) {
+          res.push(item)
+        }
+      }
+    } else if (typeof predicate === 'object') {
+
+    } else if (typeof predicate === 'function') {
+      for (let item of array) {
+        if (predicate(item)) {
+          res.push(item)
+        }
+      }
+    }
+    return res
+  }
+  // filter(array, 'n')
+  // filter(array, it=>it['n'])
+
+  // filter(array, ['active', true])
+  // filter(array, it => it['active'] === true)
+
+  // filter(array, {'active': true, 'n': 8})
+  // filter(array, it => it['active'] === true && it['n'] === 8)
+
+
+  function filter(array, predicate) {
+    let func = predicate
+    if (typeof predicate  === 'string') {
+      func = function(it) {
+        return it[predicate]
+      }
+    } else if (Array.isArray(predicate)) {
+      func = function(it) {
+        return it[ predicate[0] ] === predicate[1]
+      }
+    } else if (typeof predicate === 'object') {
+      func = function(it) {
+        for(let key in predicate) {
+          if (it[key] !== predicate[key]) {
+            return false
+          }
+        }
+        return true
+      }
+    }
+    let res = []
+    for (let item of array) {
+      if (func(item)) {
+        res.push(item)
+      }
+    }
+    return res
+  }
+
+  function filter(array, predicate) {
+    iteratee(predicate)
+    let res = []
+    for (let i = 0; i < array.length; i++) {
+      if(func[array[i]], i, array) {
+        res.push(array[i])
+      }
+    }
+    return res
+  }
+
+  function turnPredicateToFunction(predicate)   {
+    let func = predicate
+    if (typeof predicate === 'string') {
+      func = function(it) {
+        return it[predicate]
+      }
+    } else if (typeof predicate === 'object') {
+      func = function(it) {
+        for (let key in predicate) {
+          if (it[key] !== predicate[key]) {
+            false
+          }
+        }
+        return true
+      }
+    } else if (Array.isArray(predicate)) {
+      func = function(it) {
+        for (let it of predicate) {
+          return it[ predicate[0] ] === predicate[1]
+        }
+      }
+    }
+    return func
+  }
+
+  function map(array, predicate) {
+    let func = turnPredicateToFunction(predicate)
+    let res = []
+
+    for (let i = 0; i < array.length; i++) {
+      res.push(func(array[i]),i,array)
+    }
+
+    return res
+  }
+
+  function matchesProperty(path, srcValue) {
+    return function(object) {
+      return isMatch(object[path], srcValue)
+    }
+  }
+
+
+  function isMatch(object, src) { //判断src 是否是object的一部分
+    for (let key in src) {
+      if (typeof src[key] === 'object') {
+        if (!isMatch(object[key], src[key])) return false
+      } else {
+          if (object[key] !== src[key]) return false
+        }
+    }
+    return true
+  }
+
+  function matches(src) {
+    return bind(isMatch, null, undefined, src)
+  }
+
+  // function matches(src) {
+  //   return bind(isMatch, null, _, src)
+  // }
+
+
+  function iteratee(predicate) {
+    let func = predicate
+    if (typeof predicate === 'string') {
+      func = property(predicate)
+    } else if (Array.isArray(predicate)) {
+      func = matchesProperty(predicate)
+    } else if (typeof predicate === 'object') {
+      func = matches(predicate)
+    }
+    return func
+  }
+
+  function property(name) {
+    return function(obj) {
+      return obj[name]
+    }
+  }
+  function matchesProperty(path, srcValue) {
+    return function(object) {
+      return isMatch(object[path], srcValue)
+    }
+  }
+  function matches(src) {
+    return bind(isMatch, null, undefined, src)
+  }
+
+
+
+  // 带跳过参数功能的函数绑定
+  function bind(func, thisArgs, ...fixedArgs) { //[undefined, 2, undefined, 3]
+    return function(...args) { //[1,0]
+      let copy = fixedArgs.slice()
+      //填空前面跳过的参数
+      for (var i = 0, j = 0; i < copy.length; i++) {
+        if (copy[i] === undefined) {
+          copy[i] = args[j]
+          j++
+        }
+      }
+      //后续参数正常放到最后
+      while(j < args.length) {
+        copy.push(args[j++])
+      }
+      return func.call(thisArgs, ...copy)
+    }
+  }
 
